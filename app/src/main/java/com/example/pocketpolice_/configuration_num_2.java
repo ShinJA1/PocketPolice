207d4fec0ba2a698f6de3eccd32a09eb7f22fc8d
ShinJA1 <jiae980627@naver.com>
2020-05-01 19:04:39 +0900

    신지애

--- app/src/main/java/com/example/pocketpolice_/configuration_num_2.java
+++ app/src/main/java/com/example/pocketpolice_/configuration_num_2.java
@@ -1,20 +1,13 @@
 package com.example.pocketpolice_;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class Home_2 extends AppCompatActivity {
-    Button button1;
-    Button button2;
-    Button button3;
+public class configuration_num_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_configuration_num_2);
     }
-
-
 }
